324b962093e006ebadd703ca9c7f10b1b917c241
sujan787 <sujanmoi787@gmail.com>
2024-01-15 01:23:22 +0530

    added new changes

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
             <div className="flex justify-center flex-col items-center gap-2">
               <p className="text-blue-400 text-3xl md:text-5xl font-bold uppercase flex gap-2">
                 <MdOutlineCleaningServices />Amazon Scrapper</p>
-              <p className="md:text-lg">Get all amazon top product form here</p>
+              <p className="md:text-lg bg-slate-400">Get a list of the top Amazon products from here.</p>
             </div>
 
             <form className="relative" onSubmit={(e) => handleSubmit(e)}>
